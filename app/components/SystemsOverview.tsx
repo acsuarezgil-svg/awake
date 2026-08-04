4d9b7eb0e3c43098362ee61f33650943ff342f9b
Drea <acsuarezgil@gmail.com>
2026-08-04 15:43:01 -0500

    center up to down

--- app/components/SystemsOverview.tsx
+++ app/components/SystemsOverview.tsx
@@ -414,7 +414,7 @@ export default function SystemsOverview() {
                         selectedCircleItem.direction,
                         selectedCircleItem.accidentalCount,
                       ).map((note) => <span key={note}>{note}</span>)
-                    : <span>No sharps or flats</span>}
+                    : null}
                 </p>
               </div>
             )}
